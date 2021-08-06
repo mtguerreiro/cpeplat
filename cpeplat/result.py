9d52be91478fa8d1995dfc878d365fd011d234bd
Mofritz <86720720+Mofritz@users.noreply.github.com>
2021-08-06 16:01:08 +0200

    New Plotting Functions to Compare Experiment Results
    - Plot Function for each data (vin, vout, vout buck, vin_buck, il, il_avg, u)
    - Plot Function to Compare different data sets from different experiments)

--- cpeplat/result.py
+++ cpeplat/result.py
@@ -9,17 +9,261 @@ import cpeplat as cpe
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import cpeplat.result as res
+
+##########-------------- Basic Setting Functions  ----------------############
+
+def _setting_labels(type_of):
+    if type_of == 'Voltage':
+        plt.xlabel("Time / ms", fontsize=12)
+        plt.ylabel("Voltage / V", fontsize=12)  
+    
+    elif type_of == 'Current':
+        plt.xlabel("Time / ms", fontsize=12)
+        plt.ylabel("Current / A", fontsize=12) 
+        
+    elif type_of == 'ControlSignal':
+        plt.xlabel("Time / ms", fontsize=12)
+        plt.ylabel("u (DutyCycle)", fontsize=12)        
+        
+def _setting_legend(leg):
+        plt.gca().legend(leg, fontsize=12)
+        
+def _setting_title(title):
+    plt.title(title, fontsize=12)
 
 
-def result_plotVout(data):
+
+##########-------------- Basic Plot Functions  -------------------############
+
+def plot_vin(data):
         
     #Expected DataType np.array
-    plt.figure(figsize=(4, 3))
-    plt.plot(data[0],data[3])
+    plt.figure(figsize=(9, 6))
+    plt.plot(data[0],data[1])
         
-    plt.xlabel("Time / ms")
-    plt.ylabel("Voltage / V")
-    #add grid
+    res._setting_labels('Voltage')
+    res._setting_title('Input Voltage')
+    plt.grid()
     return
     
+def plot_vin_buck(data):
+
+    #Expected DataType np.array
+    plt.figure(figsize=(9, 6))
+    plt.plot(data[0],data[2])
         
+    res._setting_labels('Voltage')
+    res._setting_title('Input Voltage Buck')
+    plt.grid()
+    return       
+
+def plot_vout(data):
+        
+    #Expected DataType np.array
+    plt.figure(figsize=(9, 6))
+    plt.plot(data[0],data[3])
+        
+    res._setting_labels('Voltage')
+    res._setting_title('Output Voltage')
+    plt.grid()
+    return
+
+def plot_vout_buck(data):
+        
+    #Expected DataType np.array
+    plt.figure(figsize=(9, 6))
+    plt.plot(data[0],data[4])
+        
+    res._setting_labels('Voltage')
+    res._setting_title('Output Voltage Buck')
+    plt.grid()
+    return
+
+def plot_il(data):
+        
+    #Expected DataType np.array
+    plt.figure(figsize=(9, 6))
+    plt.plot(data[0],data[5])
+        
+    res._setting_labels('Current')
+    res._setting_title('Output Current')
+    plt.grid()
+    return
+
+def plot_il_avg(data):
+        
+    #Expected DataType np.array
+    plt.figure(figsize=(9, 6))
+    plt.plot(data[0],data[6])
+        
+    res._setting_labels('Current')
+    res._setting_title('Output Current Average')
+    plt.grid()
+    return
+
+def plot_u(data):
+        
+    #Expected DataType np.array
+    plt.figure(figsize=(9, 6))
+    plt.plot(data[0],data[7])
+        
+    res._setting_labels('ControlSignal')
+    res._setting_title('Control Signal u')
+    plt.grid()
+    return
+
+##########-------------- Comparision Plot Functions  -------------############
+
+def plot_compare_vin(data, leg = ['Controler_1','Controler_2']):
+    """ Compares Data from experiments and plots it in one Figure
+    
+    Expect: numpy Matrix
+    Example: plot_compare([data1, data2],['pid_1', 'pid_2'])
+    """
+    
+    plt.figure(figsize=(9, 6))
+    
+    for i in data:
+        plt.plot(i[0],i[1])
+        
+    res._setting_labels('Voltage')
+    res._setting_title('Input Voltage')
+    plt.gca().legend(leg)
+    plt.grid()
+
+def plot_compare_vin_buck(data, leg = ['Controler_1','Controler_2']):
+    """ Compares Data from experiments and plots it in one Figure
+    
+    Expect: numpy Matrix
+    Example: plot_compare([data1, data2],['pid_1', 'pid_2'])
+    """
+    
+    plt.figure(figsize=(9, 6))
+    
+    for i in data:
+        plt.plot(i[0],i[2])
+        
+    res._setting_labels('Voltage')
+    res._setting_title('Input Voltage Buck')
+    plt.gca().legend(leg)
+    plt.grid()
+    
+def plot_compare_vout(data, leg = ['Controler_1','Controler_2']):
+    """ Compares Data from experiments and plots it in one Figure
+    
+    Expect: numpy Matrix
+    Example: plot_compare([data1, data2],['pid_1', 'pid_2'])
+    """
+    
+    plt.figure(figsize=(9, 6))
+    
+    for i in data:
+        plt.plot(i[0],i[3])
+        
+    res._setting_labels('Voltage')
+    res._setting_title('Output Voltage')
+    plt.gca().legend(leg)
+    plt.grid()
+
+    
+def plot_compare_vout_buck(data, leg = ['Controler_1','Controler_2']):
+    """ Compares Data from experiments and plots it in one Figure
+    
+    Expect: numpy Matrix
+    Example: plot_compare([data1, data2],['pid_1', 'pid_2'])
+    """
+    
+    plt.figure(figsize=(9, 6))
+    
+    for i in data:
+        plt.plot(i[0],i[4])
+        
+    res._setting_labels('Voltage')
+    res._setting_title('Output Voltage Buck')
+    plt.grid()
+    
+def plot_compare_il(data, leg = ['Controler_1','Controler_2']):
+    """ Compares Data from experiments and plots it in one Figure.
+    
+    Expect: numpy Matrix
+    Example: plot_compare([data1, data2],['pid_1', 'pid_2'])
+    """
+    
+    plt.figure(figsize=(9, 6))
+    
+    for i in data:
+        plt.plot(i[0],i[5])
+        
+    res._setting_labels('Current')
+    res._setting_title('Output Current')
+    plt.gca().legend(leg)
+    plt.grid()
+    
+def plot_compare_il_avg(data, leg = ['Controler_1','Controler_2']):
+    """ Compares Data from experiments and plots it in one Figure
+    
+    Expect: numpy Matrix
+    Example: plot_compare([data1, data2])
+    """
+    
+    plt.figure(figsize=(9, 6))
+    
+    for i in data:
+        plt.plot(i[0],i[6])
+        
+    res._setting_labels('Current')
+    res._setting_title('Output Current Average')
+    plt.gca().legend(leg)
+    plt.grid()
+    
+def plot_compare_u(data, leg = ['Controler_1','Controler_2']):
+    """ Compares Data from experiments and plots it in one Figure
+    
+    Expect: numpy Matrix
+    Example: plot_compare([data1, data2],['pid_1', 'pid_2'])
+    """
+    
+    plt.figure(figsize=(9, 6))
+    
+    for i in data:
+        plt.plot(i[0],i[7])
+        
+    res._setting_labels('ControlSignal')
+    plt.gca().legend(leg)
+    plt.grid()   
+    
+def plot_compare_all(data, leg = ['Controler_1','Controler_2']):
+    """ Compares Data from experiments and plots it in one Figure
+    
+    Expect: numpy Matrix
+    Example: plot_compare([data1, data2],['pid_1', 'pid_2'])
+    """
+    
+    plt.figure(figsize=(16, 9))
+    plt.subplot(1, 3, 1)
+    for i in data:
+        plt.plot(i[0],i[3])
+        
+    res._setting_labels('Voltage')
+    res._setting_title('Output Voltage')
+    plt.gca().legend(leg)
+    plt.grid()   
+    
+    plt.subplot(1, 3, 2)
+    for i in data:
+        plt.plot(i[0],i[6])
+        
+    res._setting_labels('Current')
+    res._setting_title('Average Current')
+    plt.gca().legend(leg)
+    plt.grid() 
+    
+    plt.subplot(1, 3, 3)
+    for i in data:
+        plt.plot(i[0],i[7])
+        
+    res._setting_labels('ControlSignal')
+    res._setting_title('Control Signal u')
+    plt.gca().legend(leg)
+    plt.grid()
